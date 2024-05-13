3fde54e84f5321546191e1c6c800e4729ac51520
Soroush Nasiriany <snasiriany@users.noreply.github.com>
2024-05-12 22:05:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author="Soroush Nasiriany, Abhiram Maddukuri, Lance Zhang, Adeet Parikh, Aaron Lo, Abhishek Joshi, Ajay Mandlekar, Yuke Zhu",
     url="https://github.com/robocasa/robocasa",
     author_email="soroush@cs.utexas.edu",
-    version="0.5.0",
+    version="0.1.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
