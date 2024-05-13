d6c03173b6b85dfee799c092a30b9c3e79ad3622
Yuke Zhu <zyk0902@gmail.com>
2024-05-13 13:29:34 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# ------------------
-# script to setup repo, download assets, and setup macros
-# ------------------
-pip install -e .
-conda install -c numba numba -y
-python robocasa/scripts/download_kitchen_assets.py
-python robocasa/scripts/setup_macros.py
