ff7b94072dbac47b2634177d1c9365b27230ec2b
snasiriany <snasiriany@gmail.com>
2024-12-23 21:08:02 -0600

    update env to exclude size sorting

--- robocasa/environments/kitchen/multi_stage/setting_the_table/size_sorting.py
+++ robocasa/environments/kitchen/multi_stage/setting_the_table/size_sorting.py
@@ -12,8 +12,8 @@ class SizeSorting(Kitchen):
         Stack the objects from largest to smallest.
     """
 
-    # exclude layout 8 because objects sometime initilize in corner area which is unreachable
-    EXCLUDE_LAYOUTS = [8]
+    # exclude layout 9 because objects sometime initilize in corner area which is unreachable
+    EXCLUDE_LAYOUTS = [9]
 
     def __init__(self, *args, **kwargs):
 
