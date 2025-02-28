49fbe8781663d5ac185723e7c471984bf8b06041
Yifei Simon Shao <yishao@seas.upenn.edu>
2025-02-27 23:11:28 -0500

    getting geom pose instead of from bodies

--- robocasa/environments/kitchen/single_stage/kitchen_doors.py
+++ robocasa/environments/kitchen/single_stage/kitchen_doors.py
@@ -1,4 +1,5 @@
 from robocasa.environments.kitchen.kitchen import *
+from robosuite.utils.transform_utils import mat2quat
 
 
 class ManipulateDoor(Kitchen):
@@ -188,44 +189,33 @@ class ManipulateDoor(Kitchen):
             np.ndarray: 3D position [x, y, z] of the handle
         """
         # Get the handle name based on the door fixture type
-        if isinstance(self.door_fxtr, SingleCabinet):
+        if (
+            isinstance(self.door_fxtr, SingleCabinet)
+            or isinstance(self.door_fxtr, Drawer)
+            or isinstance(self.door_fxtr, Microwave)
+        ):
             handle_name = self.door_fxtr.handle_name
         elif isinstance(self.door_fxtr, HingeCabinet):
             # For double doors, you might need to choose which handle
             handle_name = self.door_fxtr.left_handle_name
-        elif isinstance(self.door_fxtr, Drawer):
-            handle_name = self.door_fxtr.handle_name
-        elif isinstance(self.door_fxtr, Microwave):
-            handle_name = "cab_micro_main_group_left_door_handle_main"
         else:
             # For other fixture types, try to find a handle site
             handle_name = f"{self.door_fxtr.name}_door_handle_handle"
 
         # If handle_name ends with "_handle", replace it with "_main"
-        if handle_name.endswith("_handle"):
-            handle_name_main = handle_name[:-7] + "_main"
-        else:
-            handle_name_main = handle_name
+        # if handle_name.endswith("_handle"):
+        #     handle_name_main = handle_name[:-7] + "_main"
+        # else:
+        #     handle_name_main = handle_name
 
         # Get the handle position from MuJoCo
 
         # try:
         # If not a site, try as a body
-        handle_body_id = self.sim.model.body_name2id(handle_name_main)
-        handle_pos = self.sim.data.body_xpos[handle_body_id]
-        handle_quat = self.sim.data.body_xquat[handle_body_id]
-        # except:
-        #     # If all else fails, estimate based on door position and size
-        #     door_pos = self.sim.data.get_body_xpos(self.door_fxtr.door_name)
-        #     # Estimate handle position based on door type and orientation
-        #     if isinstance(self.door_fxtr, SingleCabinet):
-        #         # For cabinet doors, handle is typically on the edge opposite to the hinge
-        #         orientation = self.door_fxtr.orientation
-        #         offset = np.array([0.15, 0.02, 0]) if orientation == "right" else np.array([-0.15, 0.02, 0])
-        #         handle_pos = door_pos + offset
-        #     else:
-        #         # Default offset for other door types
-        #         handle_pos = door_pos + np.array([0, 0.05, 0])
+        handle_geom_id = self.sim.model.geom_name2id(handle_name)
+        handle_pos = self.sim.data.geom_xpos[handle_geom_id]
+        handle_quat = self.sim.data.geom_xmat[handle_geom_id].reshape(3, 3)
+        handle_quat = mat2quat(handle_quat)
 
         return handle_pos, handle_quat
 
